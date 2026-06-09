e3c8dea16c60a26ce394720090cddb01dcc968b1
El Piamonte <dev@elpiamonte.com>
2026-06-09 09:22:02 -0300

    fix tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,3 @@
-import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
   safelist: [
@@ -44,4 +43,4 @@ export default {
     },
   },
   plugins: [],
-} satisfies Config;
+}
