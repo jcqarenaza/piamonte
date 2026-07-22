3ea2f7636e6affb6fc958f8ac897930b4a695218
El Piamonte <dev@elpiamonte.com>
2026-07-22 18:08:42 -0300

    fix: } extra en ComprasClient que rompía el build

--- components/compras/ComprasClient.tsx
+++ components/compras/ComprasClient.tsx
@@ -564,7 +564,6 @@ export default function ComprasClient() {
             pendiente_nc: false,
           })
         }
-        }
       }
 
       await supabase.from('comprobantes_compra').update({ estado: 'procesado' }).eq('id', comp.id)
